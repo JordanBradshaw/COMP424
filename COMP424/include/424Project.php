1393901632df76f541061190e67ce29fdd6aa580
Jordan Bradshaw <JordanBradshaw27@aol.com>
2020-11-04 19:05:27 -0800

    remove extra folder

--- COMP424/include/424Project.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "demo424";
-$db = "COMP424";
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-if (!$conn){
-die("Connection failed: ".mysqli_connect_error());
-}
