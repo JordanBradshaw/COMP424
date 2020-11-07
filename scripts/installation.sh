3870a53c2d9a6268c6fbbd709b451f19ec80963e
admin <admin@admins-MacBook-Pro.local>
2020-11-07 15:13:18 -0800

    installation commit

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -14,29 +14,29 @@ DEBIAN_FRONTEND=noninteractive apt full-upgrade -y # Upgrading software on the o
 DEBIAN_FRONTEND=noninteractive apt install -y openssh-server apache2 mysql-server php libapache2-mod-php php-mysql php-curl dnsutils # Installing required software
 sed -i -e 's/index.html/index.php index.html/g' /etc/apache2/mods-enabled/dir.conf # Adding index.php as an option for Apache2
 systemctl restart apache2 # Restarting Apache2 to make the above configuration take effect
-mysql -e "create user 'webserver'@'localhost' identified by 'password123';" # Creating a user for the webserver
-mysql -e "create database webserver;" # Creating a database for the webserver
-mysql -e "create table webserver.users(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, birthdate DATE NOT NULL, email VARCHAR(100) NOT NULL UNIQUE, sec_question VARCHAR(100) NOT NULL, sec_answer VARCHAR(255) NOT NULL, last_login DATE, num_logins INT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, activated BOOLEAN NOT NULL DEFAULT 0, totp VARCHAR(16));" # Creating a table to store user credentials
-mysql -e "create table webserver.lockout(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) NOT NULL UNIQUE, num_fails INT DEFAULT 0)" # Creating separate table for lockout system
-mysql -e "create table webserver.email_verification(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(100) NOT NULL UNIQUE, ver_key VARCHAR(255) NOT NULL);" # Creating separate table for email verification
-mysql -e "create table webserver.password_reset(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(100) NOT NULL UNIQUE, ver_key VARCHAR(255) NOT NULL);" # Creating separate table for password resets
-mysql -e "create table webserver.sign_on_logs(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100) NOT NULL, success BOOLEAN NOT NULL, time DATETIME DEFAULT CURRENT_TIMESTAMP);" # Creating separate table for sign on logs
-mysql -e "grant insert on webserver . users to 'webserver'@'localhost';" # Granting permission to insert registration data to the webserver user
-mysql -e "grant update on webserver . users to 'webserver'@'localhost';" # Granting permission to update registration data to the webserver user
-mysql -e "grant select on webserver . users to 'webserver'@'localhost';" # Granting permission to view registration data to the webserver user
-mysql -e "grant insert on webserver . email_verification to 'webserver'@'localhost';" # Granting permission to insert data to the webserver user
-mysql -e "grant update on webserver . email_verification to 'webserver'@'localhost';" # Granting permission to update data to the webserver user
-mysql -e "grant select on webserver . email_verification to 'webserver'@'localhost';" # Granting permission to view data to the webserver user
-mysql -e "grant delete on webserver . email_verification to 'webserver'@'localhost';" # Granting permission to delete data to the webserver user
-mysql -e "grant insert on webserver . lockout to 'webserver'@'localhost';" # Granting permission to insert data to the webserver user
-mysql -e "grant update on webserver . lockout to 'webserver'@'localhost';" # Granting permission to update data to the webserver user
-mysql -e "grant select on webserver . lockout to 'webserver'@'localhost';" # Granting permission to view data to the webserver user
-mysql -e "grant insert on webserver . password_reset to 'webserver'@'localhost';" # Granting permission to insert data to the webserver user
-mysql -e "grant update on webserver . password_reset to 'webserver'@'localhost';" # Granting permission to update data to the webserver user
-mysql -e "grant select on webserver . password_reset to 'webserver'@'localhost';" # Granting permission to view data to the webserver user
-mysql -e "grant delete on webserver . password_reset to 'webserver'@'localhost';" # Granting permission to delete data to the webserver user
-mysql -e "grant insert on webserver . sign_on_logs to 'webserver'@'localhost';" # Granting permission to insert logs to the webserver user
-mysql -e "grant select on webserver . sign_on_logs to 'webserver'@'localhost';" # Granting permission to view logs to the webserver user
+mysql -e "create user '424Admin'@'localhost' identified by '484Group';" # Creating a user for the webserver
+mysql -e "create database 424Admin;" # Creating a database for the webserver
+mysql -e "create table 424Admin.tbl_users(user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(120) NULL, password VARCHAR(200) NULL, security_question MEDIUMTEXT NULL, security_answer TEXT NULL, profile_name VARCHAR(100) NULL, google_auth_code VARCHAR(16) NULL, created_at VARCHAR(15) NULL, last_login VARCHAR(15), total_logins INT NOT NULL;" # Creating a table to store user credentials
+mysql -e "create table 424Admin.lockout(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) NOT NULL UNIQUE, num_fails INT DEFAULT 0)" # Creating separate table for lockout system
+mysql -e "create table 424Admin.email_verification(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(100) NOT NULL UNIQUE, ver_key VARCHAR(255) NOT NULL);" # Creating separate table for email verification
+mysql -e "create table 424Admin.password_reset(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(100) NOT NULL UNIQUE, ver_key VARCHAR(255) NOT NULL);" # Creating separate table for password resets
+mysql -e "create table 424Admin.sign_on_logs(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100) NOT NULL, success BOOLEAN NOT NULL, time DATETIME DEFAULT CURRENT_TIMESTAMP);" # Creating separate table for sign on logs
+mysql -e "grant insert on 424Admin . users to '424Admin'@'localhost';" # Granting permission to insert registration data to the webserver user
+mysql -e "grant update on 424Admin . users to '424Admin'@'localhost';" # Granting permission to update registration data to the webserver user
+mysql -e "grant select on 424Admin . users to '424Admin'@'localhost';" # Granting permission to view registration data to the webserver user
+mysql -e "grant insert on 424Admin . email_verification to '424Admin'@'localhost';" # Granting permission to insert data to the webserver user
+mysql -e "grant update on 424Admin . email_verification to '424Admin'@'localhost';" # Granting permission to update data to the webserver user
+mysql -e "grant select on 424Admin . email_verification to '424Admin'@'localhost';" # Granting permission to view data to the webserver user
+mysql -e "grant delete on 424Admin . email_verification to '424Admin'@'localhost';" # Granting permission to delete data to the webserver user
+mysql -e "grant insert on 424Admin . lockout to '424Admin'@'localhost';" # Granting permission to insert data to the webserver user
+mysql -e "grant update on 424Admin . lockout to '424Admin'@'localhost';" # Granting permission to update data to the webserver user
+mysql -e "grant select on 424Admin . lockout to '424Admin'@'localhost';" # Granting permission to view data to the webserver user
+mysql -e "grant insert on 424Admin . password_reset to '424Admin'@'localhost';" # Granting permission to insert data to the webserver user
+mysql -e "grant update on 424Admin . password_reset to '424Admin'@'localhost';" # Granting permission to update data to the webserver user
+mysql -e "grant select on 424Admin . password_reset to '424Admin'@'localhost';" # Granting permission to view data to the webserver user
+mysql -e "grant delete on 424Admin . password_reset to '424Admin'@'localhost';" # Granting permission to delete data to the webserver user
+mysql -e "grant insert on 424Admin . sign_on_logs to '424Admin'@'localhost';" # Granting permission to insert logs to the webserver user
+mysql -e "grant select on 424Admin . sign_on_logs to '424Admin'@'localhost';" # Granting permission to view logs to the webserver user
 mysql -e "flush privileges;" # Updating the privileges to make the above configuration take effect
 
 rm -rf /var/www/*
