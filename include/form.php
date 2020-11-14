33e948cf2b8f1c24d6babe731e58f115dc7eefe6
Jordan Bradshaw <JordanBradshaw27@aol.com>
2020-11-14 12:06:08 -0800

    Spring Cleaning

--- include/form.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['captcha']) and $_POST['my-captcha'] == $_SESSION['captcha']) {
-    unset($_SESSION['captcha']);
-    echo "Correct captcha!";
-} else {
-    echo ("nope!");
-}
